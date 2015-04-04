5abe32f96e84e29ed4c59830c26b2e95ede93397
mark <mcdermottsolutions@gmail.com>
2015-04-04 12:49:39 -0500

    pasted in right client.js code

--- public/client.js
+++ public/client.js
@@ -1,12 +1,13 @@
-{
-  "name": "socket-chat-example",
-  "version": "0.0.1",
-  "description": "my first socket.io app",
-  "dependencies": {
-    "body-parser": "^1.12.2",
-    "express": "^4.10.2",
-    "path": "^0.11.14",
-    "socket.io": "^1.3.5",
-    "twilio": "^1.11.1"
-  }
-}
+var socket = io();
+
+$('form').submit(function(){
+  var clientMsg = $('#m').val();
+  socket.emit('webclient message', clientMsg);
+  $('#messages').append($('<li>').text('you: ' + clientMsg));
+  $('#m').val('');
+  return false;
+});
+
+socket.on('sms message', function(smsMsg){
+  $('#messages').append($('<li>mark: ').text('mark (from sms): ' + smsMsg));
+});
