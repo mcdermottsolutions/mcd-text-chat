811c75e3f60956b049c96d8d58c4d58c82c81a2f
mark <mcdermottsolutions@gmail.com>
2015-04-04 11:39:21 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var bodyParser = require('body-parser'),
+
+myTwilioNumber = '+18305496287',
+myCellNumber = '+15128203772',
+twilio = require('twilio')(process.env.TWILIO_ACCOUNT_SID,process.env.TWILIO_AUTH_TOKEN); 
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', function(socket){
+	
+	socket.on('webclient message', function(msg){
+		twilio.sendMessage({
+			to: myCellNumber, 
+			from: myTwilioNumber,
+			body: msg 
+		});
+	});
+
+	app
+	 	.use(bodyParser.urlencoded({extended: true}))
+		.post('/incoming', function(req, res) {
+			var smsMessage = req.body.Body;
+			console.log(smsMessage);
+			io.emit('sms message', smsMessage);
+		});
+
+});
+
+http.listen(8080, function(){
+	console.log('listening on *:8080');
+});
---
+++ public/client.js
@@ -0,0 +1,12 @@
+{
+  "name": "socket-chat-example",
+  "version": "0.0.1",
+  "description": "my first socket.io app",
+  "dependencies": {
+    "body-parser": "^1.12.2",
+    "express": "^4.10.2",
+    "path": "^0.11.14",
+    "socket.io": "^1.3.5",
+    "twilio": "^1.11.1"
+  }
+}
